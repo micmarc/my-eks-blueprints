9663b6b11de789d646152576e786f09f15e8aca6
Michael Marcus <mmarcus@captechconsulting.com>
2022-05-18 18:52:43 +0000

    Setting up EKS Blueprints deployment pipeline

--- bin/my-eks-blueprints.ts
+++ bin/my-eks-blueprints.ts
@@ -1,6 +1,7 @@
 // bin/my-eks-blueprints.ts
 import * as cdk from 'aws-cdk-lib';
 import ClusterConstruct from '../lib/my-eks-blueprints-stack';
+import PipelineConstruct from '../lib/pipeline'; // IMPORT OUR PIPELINE CONSTRUCT
 
 
 const app = new cdk.App();
@@ -9,3 +10,4 @@ const region = process.env.CDK_DEFAULT_REGION;
 const env = { account, region }
 
 new ClusterConstruct(app, 'cluster', { env });
+new PipelineConstruct(app, 'pipeline', { env });
