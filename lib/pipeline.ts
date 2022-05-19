e6b7d74613825e4e3eb693dec23eb7555149bbd8
Michael Marcus <mmarcus@captechconsulting.com>
2022-05-19 13:18:23 +0000

    adding teams

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -1,38 +1,39 @@
 // lib/pipeline.ts
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as blueprints from '@aws-quickstart/eks-blueprints';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as blueprints from "@aws-quickstart/eks-blueprints";
+import { TeamPlatform, TeamApplication } from "../teams"; // HERE WE IMPORT TEAMS
 
 export default class PipelineConstruct extends Construct {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps){
-    super(scope,id)
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id);
 
     const account = props?.env?.account!;
     const region = props?.env?.region!;
 
     const blueprint = blueprints.EksBlueprint.builder()
-    .account(account)
-    .region(region)
-    .addOns()
-    .teams();
-  
+      .account(account)
+      .region(region)
+      .addOns()
+      .teams(new TeamPlatform(account), new TeamApplication('burnham',account));
+
     blueprints.CodePipelineStack.builder()
       .name("eks-blueprints-workshop-pipeline")
       .owner("micmarc")
       .repository({
-          repoUrl: 'my-eks-blueprints',
-          credentialsSecretName: 'github-token',
-          targetRevision: 'master'
+        repoUrl: "my-eks-blueprints",
+        credentialsSecretName: "github-token",
+        targetRevision: "master",
       })
       // WE ADD THE STAGES IN WAVE FROM THE PREVIOUS CODE
       .wave({
         id: "envs",
         stages: [
-          { id: "dev", stackBuilder: blueprint.clone('us-west-2')},
-          { id: "test", stackBuilder: blueprint.clone('us-east-2')},
-          { id: "prod", stackBuilder: blueprint.clone('us-east-1')}
-        ]
+          { id: "dev", stackBuilder: blueprint.clone("us-west-2") },
+          { id: "test", stackBuilder: blueprint.clone("us-east-2") },
+          { id: "prod", stackBuilder: blueprint.clone("us-east-1") },
+        ],
       })
-      .build(scope, id+'-stack', props);
+      .build(scope, id + "-stack", props);
   }
 }
